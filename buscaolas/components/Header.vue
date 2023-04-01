9f9f511e658d7f19317ba83a82d2e9665a88cacc
kai121814 <kaita.ostia@gmail.com>
2023-04-01 15:13:54 +0800

    first commit joshii

--- buscaolas/components/Header.vue
+++ buscaolas/components/Header.vue
@@ -6,7 +6,7 @@
         <div class="flex mx-auto w-9/12">
             <span class="w-full lg:w-auto lg:mt-3 mt-2.5 lg:ml-0 ml-8">
                 <NuxtLink to="/">
-                    <img src="~/asset/logo/BuscaolasLogo1.png">
+                    <img src="~/assets/logo/BuscaolasLogo1.png">
                 </NuxtLink>
             </span>
             <span class="lg:flex lg:static absolute z-10 w-full uppercase text-center text-2xl lg:text-sm text-white lg:my-auto menu mt-10 lg:pt-0 pt-5 pb-5" :class="[toggleMenu ? 'block' : 'hidden']">
