737aea0030bcbdee57fdc3dc7f95271698a90aa3
kai121814 <kaita.ostia@gmail.com>
2023-04-01 15:09:59 +0800

    first commit joshii

--- buscaolas/pages/index.vue
+++ buscaolas/pages/index.vue
@@ -1,2 +1,13 @@
 <template>
+    <div class="overflow-x-hidden">
+        <client-only>
+            <Header />
+            <div class="mx-auto w-11/12">
+                <p>
+                    Content
+                </p>
+            </div>
+            
+        </client-only>        
+    </div>
 </template>
