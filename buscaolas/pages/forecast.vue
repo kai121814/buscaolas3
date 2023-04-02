08eb48401fac421c1a558d0750aa77c9e257a409
jorenlee24 <jo@bluthrive.com>
2023-04-02 17:00:27 +0800

    forecast added

---
+++ buscaolas/pages/forecast.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div class="overflow-x-hidden">
+      <client-only>
+        <Header />
+        <div class="mx-auto w-11/12">
+
+        </div>
+ </div>
+</template>
+
+<script scoped>
+ </script>
+
+<style lang="scss" scoped>
+
+</style>
