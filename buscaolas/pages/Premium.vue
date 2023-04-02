b88f3744db459d284ca2e8744986e2eca074bad1
kai121814 <kaita.ostia@gmail.com>
2023-04-02 17:46:35 +0800

    unfinished joshii premium

--- buscaolas/pages/Premium.vue
+++ buscaolas/pages/Premium.vue
@@ -21,22 +21,19 @@
                 </div>
                 <div class="flex mx-auto w-9/12">
                     <div class="flex mx-auto w-full">
-                        <div class="w-full flex lg:py-7 py-10">
-                            <div>
-                                <form action="/" class="w-full">
-                                    <p class="font-bold uppercase">cambiar contraseña</p>
-                                    <div>
-                                        <input class="w-full" type="actual-contraseña" id="actual-contraseña" name="actual-contraseña" value="actual-contraseña"><br>
-                                    </div>
-                                    <div>
-                                        <input type="text" id="lname" name="lname" value="Doe">
-                                    </div>
-                                    <div>
-                                        <input type="submit" value="Submit">
-                                    </div>
-                                    
-                                </form> 
-                            </div>  
+                        <div class="w-full flex lg:py-7 py-10 bg-black items-center justify-center h-screen">
+                            <form action="/" class="max-w-md w-full bg-white">
+                                <p class="font-bold uppercase">cambiar contraseña</p>
+                                <div>
+                                    <input type="actual-contraseña" id="actual-contraseña" name="actual-contraseña" value="actual-contraseña"><br>
+                                </div>
+                                <div>
+                                    <input type="text" id="lname" name="lname" value="Doe">
+                                </div>
+                                <div>
+                                    <input type="submit" value="Submit">
+                                </div> 
+                            </form> 
                         </div>
                         <div class="w-full flex mx-auto">
                              <button type="button" class="font-bold text-center bg-white text-black mx-auto">VOLVER</button>
