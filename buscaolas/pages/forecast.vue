7665155e8dd1c4a24a698fda1d82e07dbacf41dd
jorenlee24 <jo@bluthrive.com>
2023-04-02 17:43:20 +0800

    forecast  update

--- buscaolas/pages/forecast.vue
+++ buscaolas/pages/forecast.vue
@@ -1,11 +1,25 @@
 <template>
   <div>
     <div class="overflow-x-hidden">
-      <client-only>
         <Header />
-        <div class="mx-auto w-11/12">
-
+        <div class="mx-auto w-11/12 w-full">
+      <div class="flex grid grid-flow-col auto-cols-max bg-gradient-to-t from-blue-900 to-teal-500 text-white ">
+        <div> <img src="~/assets/thumbnails/surf.png" alt="surf"></div>
+        <div class="p-5">
+          <p class="uppercase font-bold text-3xl"> CORRE MEJORES OLAS, MUCHO MÁS </p>
+          <p class="uppercase font-bold text-3xl">SEGUIDO.</p>
+          <p class="uppercase">Cámaras exclusivas  |  Reportes personalizados  |  Videos sin anuncios |  Descuentos exclusivos</p>
+          <p class="uppercase font-bold text-2xl">POR SOLO $2.500 MENSUALES</p>
+          <div >
+            <button class="bg-teal-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 uppercase">
+          p r u e b a    g r a t i s
+        </button>
+        <p class="grid grid-flow-col">( por 14 días )</p>
         </div>
+         </div>
+      </div>
+        </div>
+ </div>
  </div>
 </template>
 
