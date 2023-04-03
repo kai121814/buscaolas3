ce2fa4607e8b538bda50d75c495c062b430d8d14
Michael Earvin Tia <mictt123@gmail.com>
2023-04-03 13:23:36 +0800

    tia update

---
+++ buscaolas/pages/forgetpassword.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="overflow-x-hidden">
+        <Header />
+        <div class="relative mx-auto w-full h-full">
+            <div>
+                <img src="~/assets/css/banners/forget.png" class="w-full" alt="forget" />
+            </div>
+        </div>
+        <div class="pt-5 bg-gradient-to-t from-[#F8C8DC] to-white">
+            <div class="w-1/3 mx-auto">
+                <p class="text-center italic text-4xl">
+                    <span class="font-bold">
+                        Con premium disfruta de increíbles beneficios
+                    </span>
+                </p>
+
+                <p class="text-center">
+                    Francisco, prueba por 14 días los increíbles beneficios que tiene para ti Premium.
+                </p>
+                <p class="text-center">1. Puedes cancelar en cualquier momento son costos ocultos ni adicionales.</p>
+                <p class="text-center">2. Tus datos bancarios quedan almacenados en la de red bancos de Chile, no en nuestros servidores por lo que es 100% seguro</p>
+            </div>
+            <div class="flex justify-center">
+                <button class="bg-orangee text-white font-bold py-2 px-4 rounded shadow-2xl uppercase ">
+                probar 14 dias gratis
+            </button>
+            </div>
+            <div class="flex justify-center pt-2 underline text-[#00A99D]">
+                <a href="#">Seguir con mi cuenta gratis</a>
+            </div>
+            <div class="cover-bg lg:visible invisible h-0 lg:h-auto mt-0 lg:-mt-28 pt-5 text-headblue">
+                <div class="cover">
+                    <div class="box pt-40 pb-10">
+                        <gopremium />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
+<style></style>
---
+++ buscaolas/pages/successful.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="overflow-x-hidden">
+        <Header />
+        <div class="relative mx-auto w-full h-full">
+            <div>
+                <img src="~/assets/css/banners/successful.png" class="w-full" alt="successful" />
+            </div>
+        </div>
+        <div class="pt-5 bg-gradient-to-t from-[#F8C8DC] to-white">
+            <div class="w-1/3 mx-auto">
+                <p class="text-center italic text-4xl">
+                    <span class="font-bold">
+                    Bienvenido a Premium
+                    </span>
+                </p>
+
+                <p class="text-center">
+                    Hemos verificado tu subscripcion y ahora eres parte de la <span class="font-bold"> familia premium.</span> Con increibles beneficios para ti.
+                </p>
+                <p class="text-center">Te recordamos que tienes 14 dias de pruebas y tu primer cobro se realizara el <span class="font-bold">20 de octubre 2021.</span> </p>
+            </div>
+            <div class="cover-bg lg:visible invisible h-0 lg:h-auto mt-0 lg:-mt-28 pt-5 text-headblue">
+                <div class="cover">
+                    <div class="box pt-40 pb-10">
+                        <gopremium />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
+<style></style>
